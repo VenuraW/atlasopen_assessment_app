fda8209b8650b389ddec6dfaa7b3996c5fbb734e
Venura <valw0002@student.monash.edu>
2022-02-28 23:03:43 +1100

    Relocating files into a components folder

--- src/index.js
+++ src/index.js
@@ -1,17 +1,18 @@
-import React, { Suspense } from 'react';
-import ReactDOM from 'react-dom';
-import { FirebaseAppProvider, SuspenseWithPerf } from 'reactfire';
-import firebaseConfig from './firebaseConfig';
-import App from './App';
-import './index.css';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import { FirebaseAppProvider, SuspenseWithPerf } from "reactfire";
+import firebaseConfig from "./firebaseConfig";
+import App from "./components/App";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.render(
-    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-        <Suspense fallback={<h3>Loading...</h3>}>
-            <React.StrictMode>
-                <App />
-            </React.StrictMode>
-        </Suspense>
-    </FirebaseAppProvider>,
-    document.getElementById('root')
+	<FirebaseAppProvider firebaseConfig={firebaseConfig}>
+		<Suspense fallback={<h3>Loading...</h3>}>
+			<React.StrictMode>
+				<App />
+			</React.StrictMode>
+		</Suspense>
+	</FirebaseAppProvider>,
+	document.getElementById("root")
 );
