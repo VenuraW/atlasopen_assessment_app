7bc7d4ca882727c92596896e09edc20af284c196
Venura <valw0002@student.monash.edu>
2022-02-28 23:04:14 +1100

    Adding new firebase info

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+﻿import { initializeApp } from "firebase/app";
+
+import "firebase/auth";
+import firebaseConfig from "./firebaseConfig";
+
+const app = initializeApp({
+	apiKey: firebaseConfig.apiKey,
+	authDomain: firebaseConfig.authDomain,
+	projectId: firebaseConfig.projectId,
+	storageBucket: firebaseConfig.storageBucket,
+	messagingSenderId: firebaseConfig.messagingSenderId,
+	appId: firebaseConfig.appId,
+});
+
+export const auth = app.auth();
+export default app;
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,12 +1,10 @@
 var firebaseConfig = {
-    //TODO Replace with own config file
-    // apiKey: "AIzaSyD7KhDYomFFrQE4dHH6DDs-SNK9rfptZ9o",
-    // authDomain: "assessment-app-41d0b.firebaseapp.com",
-    // databaseURL: "https://assessment-app-41d0b.firebaseio.com",
-    // projectId: "assessment-app-41d0b",
-    // storageBucket: "assessment-app-41d0b.appspot.com",
-    // messagingSenderId: "418813227371",
-    // appId: "1:418813227371:web:22828bd50e32150598dc2e"
+	apiKey: "AIzaSyCjpoLGJ7e94gSmYg4BUzZL74c7fYrWX6k",
+	authDomain: "assessment-app-v.firebaseapp.com",
+	projectId: "assessment-app-v",
+	storageBucket: "assessment-app-v.appspot.com",
+	messagingSenderId: "513033962651",
+	appId: "1:513033962651:web:f3a376d656952940eff23e",
 };
 
 export default firebaseConfig;
