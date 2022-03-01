852815e1815f09d9c5c449d373c14bedeb8b1444
Venura <valw0002@student.monash.edu>
2022-03-01 14:36:14 +1100

    Chat page

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,33 +1,41 @@
 import React, { useEffect, useState } from "react";
-
-//HINT https://github.com/FirebaseExtended/reactfire
-import { useUser, useFirestoreCollectionData, useFirestore } from 'reactfire';
-import Logout from './Logout';
-import 'firebase/auth'
+import { Card, Button, Alert } from "react-bootstrap";
+import Logout from "./Logout";
+import "firebase/auth";
+import { useAuth } from "../contexts/AuthContext";
 
 //HINT
-import { BrowserRouter as Router, Switch, Route, Link, useParams, useHistory } from 'react-router-dom';
-import {TextField, Button, Grid, Typography} from "@material-ui/core";
+import {
+	BrowserRouter as Router,
+	Switch,
+	Route,
+	Link,
+	useParams,
+	useHistory,
+} from "react-router-dom";
 
 export default function Chat() {
+	const [error, setError] = useState("");
+	const { currentUser, logout } = useAuth();
 
-    // TODO: Display messages from chat and submit messages
+	function handleLogout() {}
 
-    return (
-        <div>
-            <h1>Chat Page</h1>
-
-            <Grid container>
-            <Grid md={12}>
-            <TextField onChange={(e) => console.log(e.target.value)}/>
-            </Grid>
-            <Grid md={12}>
-            <Button variant="contained">Submit</Button>
-            </Grid>
-            </Grid>
-
-            <Logout />
-        </div>
-    )
-
+	return (
+		<>
+			<Card>
+				<Card.Body className="text-center">
+					<h1>Chat Page</h1>
+					{error && <Alert variant="danger">{error}</Alert>}
+					<div className="d-flex flex-column text-center">
+						<strong>{currentUser.displayName}</strong>
+						<strong>{currentUser.email}</strong>
+					</div>
+					<Button variant="contained">Submit</Button>
+				</Card.Body>
+			</Card>
+			<Button type="submit" onClick={handleLogout} className="w-100 mt-3">
+				Log Out
+			</Button>
+		</>
+	);
 }
